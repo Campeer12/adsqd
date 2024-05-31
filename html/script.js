554f6909e08a557edad8462919fda96ec0861ab5
Campeer12 <45078782+Campeer12@users.noreply.github.com>
2024-06-01 01:57:50 +0200

    Add files via upload

---
+++ html/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const searchButton = document.querySelector(".search-bar button");
+    searchButton.addEventListener("click", () => {
+        alert("جاري البحث عن المنتج...");
+    });
+});
